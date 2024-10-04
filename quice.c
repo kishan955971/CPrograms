71ce44e5691911378508895d9f6324347a8dafb8
kishan955971 <kishan221314z@gmail.com>
2024-10-04 15:32:51 +0530

    Quiz
    Making a MCQ  quiz.

---
+++ quice.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int givenanswer;
+    int correctanswer = 3;
+    printf("What is capital of india?");
+    printf("\n");
+    printf("1. lakhanou");
+    printf("\n");
+    printf("2. panjab");
+    printf("\n");
+    printf("3. new delhi");
+    printf("\n");
+    printf("4. Error");
+    while (1)
+    {
+
+        printf("\nPlease choose a number between 1 and 4\n");
+        scanf("%d", &givenanswer);
+        if (givenanswer < 1 || givenanswer > 4)
+        {
+            printf("error");
+            continue;
+        }
+        break;
+    }
+    if (correctanswer == givenanswer)
+    {
+        printf("right");
+    }
+    else
+        printf("error");
+
+    return 0;
+}
