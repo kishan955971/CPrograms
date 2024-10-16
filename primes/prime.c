abbad8e341dc4ac486bece148279f423ac5b1c36
kishan955971 <kishan221314z@gmail.com>
2024-10-16 09:46:41 +0530

    825 loop count now
    Primes upto 500 are 95 in 925 steps

--- primes/prime.c
+++ primes/prime.c
@@ -2,14 +2,15 @@
 #include<math.h>
 int main()
 {
-	int i,n=500,j,isprime,limit,count=0,loopcount=0;
+	int i,n=500,j,isprime,limit,count=0,loopcount=0,t;
 //printf("Enter value of number=");
 //	scanf("%d",&n);
-printf("%d,",2);
-count++;
-for(i=3;i<=n;i=i+2)
+printf("2,3");
+count=2;
+t=4;
+for(i=5;i<=n;i=i+t)
 {
-
+t=6-t;
 	limit=(int)sqrt(i);
 	isprime=1;
 	for(j=3;j<=limit;j=j+2)
